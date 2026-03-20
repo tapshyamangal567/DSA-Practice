2ffd81cd770ddde74249482f7539519f2dec4474
Tapshya Mangal <mangaltapasya@gmail.com>
2026-03-20 20:10:12 +0530

    Add searchInsert function for finding insert position
    Implement binary search to find the insert position for a target in a sorted array.

---
+++ Arrays/problem9_search_insert_position.cpp
@@ -0,0 +1,30 @@
+// Platform : Leetcode
+// Problem : Search insert position 
+// Difficulty : Easy
+// TC : O(n)
+// SC : O(1)
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low=0;
+        int high = n-1;
+        while(low <= high) {
+            int mid = low + (high -low)/2;
+
+            if(nums[mid] == target ) {
+                return mid;
+            } 
+            else if (nums[mid] < target) {
+                low = mid+1;
+            }
+            else {
+                high = mid -1;
+            }
+
+    
+        }
+        return low;
+         
+    }
