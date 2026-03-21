ebf5aa55fb837eb8be4224afd068b746ad2e2c7f
Tapshya Mangal <mangaltapasya@gmail.com>
2026-03-21 17:07:35 +0530

    Add removeElement function for array value removal
    Implement removeElement function to remove specified value from array.

---
+++ Arrays/problem10_remove_element.cpp
@@ -0,0 +1,19 @@
+// Platform : Leetcode 
+// Problem : remove element 
+// Difficulty : Easy
+// TC : O(N)
+// SC : O(1)
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+       int k=0;
+       for(int i=0 ;i<nums.size();i++) {
+        if(nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+       }
+       return k;
+    }
+};
