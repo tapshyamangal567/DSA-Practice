06bff875110acfc16f4e26121f4025b2625c7892
Tapshya Mangal <mangaltapasya@gmail.com>
2026-03-24 20:10:46 +0530

    Add solution for array partition problem
    Implement a solution for partitioning an array into pairs and summing the minimums.

---
+++ Arrays/problem14_Array_partition.cpp
@@ -0,0 +1,18 @@
+// Platform : leetcode
+// Problem : partition the array
+// Difficulty : easy
+// TC ; O(n log n)
+// SC : O(1)
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+
+        int sum=0;
+        for(int i=0;i<nums.size(); i+=2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
