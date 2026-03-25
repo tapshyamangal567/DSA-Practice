05204138b99d5776aed52e20e74d149df73fc8eb
Tapshya Mangal <mangaltapasya@gmail.com>
2026-03-25 20:26:10 +0530

    Add solution for maximum consecutive ones problem
    Implemented a solution to find the maximum number of consecutive 1's in a binary array.

---
+++ Arrays/problem15_maximum_consecutive_ones.cpp
@@ -0,0 +1,27 @@
+// Platform : takeuforward
+// problem : find maximum consecutive 1's
+// difficulty : easy 
+// TC : O(N)
+// SC : O(1)
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n = nums.size();
+        int count =0;
+        int max_count =0;
+        for(int i=0 ; i<n ;i++) {
+            if(nums[i] == 1) {
+                count++;
+                max_count = max(count , max_count);
+            }
+            else {
+                count =0;
+            }
+            
+
+        }
+        return max_count;
+    
+    }
+};
